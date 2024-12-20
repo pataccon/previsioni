cd68d16b978f5f738d9f94208a8227b594e0aff5
daniele.sbrighi <daniele.sbrighi.stud@ispascalcomandini.it>
2024-12-20 09:42:02 +0100

    spostamento in git del progetto

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+internal class Program
+{
+    private static async Task Main()
+    {
+        HttpClient client=new HttpClient();
+        client.BaseAddress=new Uri("http://api.openweathermap.org");
+
+        HttpResponseMessage risposta = await client.GetAsync($"/data/2.5/weather?lat=44.261667&lon=12.358889&appid=5925c2e653052a57863fcaf5ac6acd81");//la risposta la da solo quando sara disponibile, rimane in attesa
+
+        var risultato=await risposta.Content.ReadAsStringAsync();
+
+
+//deserializzazione da jason
+        var messaggio= JsonConvert.DeserializeObject<dynamic>(risultato)!;
+
+//estrazione dell'id
+        float temperatura =(float)(messaggio!.main.temp - 273.15);
+
+        string cose=messaggio.weather[0].description;
+
+        Console.WriteLine($"temperatura corrente a {messaggio.name}: {temperatura}\r\n descrizione:{cose}");
+        
+    }
+}
---
+++ dettagli esercizio/dettagli per l'es.md
@@ -0,0 +1,4 @@
+indirizzo del sito: https://api.openweathermap.org/data/2.5/weather?lat=44.261667&lon=12.358889&appid=5925c2e653052a57863fcaf5ac6acd81
+
+aggiunta del pacchetto aggiuntivo usato:
+```dotnet add package Newtonsoft.json```
